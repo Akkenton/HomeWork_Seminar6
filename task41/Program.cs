b5449fb2dfccf03858a580fcd2bc846a69e542d6
Akkenton <akkenton.msk@gmail.com>
2022-12-29 19:05:41 +0300

    Обернул в функцию решение

--- task41/Program.cs
+++ task41/Program.cs
@@ -6,36 +6,45 @@
 
 // РЕШЕНИЕ №1 - с объявлением массива ---------------------
 
-// Console.Clear();
-// System.Console.WriteLine("Количество чисел: "); // Так как в задаче есть переменная М, логично предположить что количество чисел задается заранее.
-// int M = int.Parse(Console.ReadLine());
-// int count = 0; // счетчик положительных чисел
-// int[] arr = new int[M]; //объявляем массив, куда складываем введенные пользователем числа
-// for (int i = 0; i < M; i++)   // заполняем массив
-// {
-//     System.Console.WriteLine("Введите число: ");
-//     arr[i] = int.Parse(Console.ReadLine());
-// }
+Console.Clear();
+System.Console.WriteLine("Количество чисел: "); // Так как в задаче есть переменная М, логично предположить что количество чисел задается заранее.
+int M = int.Parse(Console.ReadLine());
+int FindPolCount(int countNum)
+{
+    int count = 0; // счетчик положительных чисел
+    int[] arr = new int[M]; //объявляем массив, куда складываем введенные пользователем числа
+    for (int i = 0; i < M; i++)   // заполняем массив
+    {
+        System.Console.WriteLine("Введите число: ");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
 
-// for (int i = 0; i < arr.Length; i++) // подсчитываем количество положительных чисел
-// {
-//     if (arr[i] > 0) count++;
-// }
-// System.Console.WriteLine($"Количество положительных чисел в массиве: {count}");
+    for (int i = 0; i < arr.Length; i++) // подсчитываем количество положительных чисел
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+System.Console.WriteLine($"Количество положительных чисел в массиве: {FindPolCount(M)}");
 
 
 // РЕШЕНИЕ №2 - без объявления массива(в задаче не сказано про использование массива в принципе) -----
-Console.Clear();
-System.Console.WriteLine("Количество чисел: ");
-int M = int.Parse(Console.ReadLine());
-int bufer = 0; // буферная переменная, для временного хранения числа от пользователя
-int count = 0; // счетчик положительных чисел
+// Console.Clear();
+// System.Console.WriteLine("Количество чисел: ");
+// int M = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < M; i++)
-{
-    System.Console.WriteLine("Введите число: ");
-    bufer = int.Parse(Console.ReadLine());
-    if (bufer > 0) count++;
-}
-System.Console.WriteLine($"Количество положительных чисел в массиве: {count}");
+// int FindPolCount(int countNum)
+// {
+//     int count = 0; // счетчик положительных чисел
+//     int bufer = 0; // буферная переменная, для временного хранения числа от пользователя
+//     for (int i = 0; i < countNum; i++)
+//     {
+//         System.Console.WriteLine("Введите число: ");
+//         bufer = int.Parse(Console.ReadLine());
+//         if (bufer > 0) count++;
+//     }
+//     return count;
+// }
+
+// System.Console.WriteLine($"Количество положительных чисел: {FindPolCount(M)}");
 // -------------------  END  ------------------------
